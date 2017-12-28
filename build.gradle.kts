aa98e2b0ec9c9033c73fc14707f23c12f2d69f52
Victor Kropp <victor.kropp@gmail.com>
2017-12-28 23:27:25 +0100

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+group = "com.github.kropp.gradle"
+version = "0.1"
+
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "0.9.9"
+}
+
+gradlePlugin {
+    (plugins) {
+        "thanks" {
+            id = "thanks"
+            implementationClass = "com.github.kropp.gradle.thanks.ThanksPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/kropp/gradle-plugin-thanks"
+    vcsUrl = "https://github.com/kropp/gradle-plugin-thanks"
+    description = "Say thanks to the libraries you depend on in form of a Github star"
+    tags = listOf("github", "star")
+    (plugins) {
+        "thanks" {
+            id = "com.github.kropp.thanks"
+            displayName = "Thanks"
+        }
+    }
+}
